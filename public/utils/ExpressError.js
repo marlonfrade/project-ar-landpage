c0c7e942c9c080c4ae9ed82ad14278084e04c392
Marlon Frade <marlonmelo98@hotmail.com>
2022-03-28 18:02:39 -0300

    working on the header section

---
+++ public/utils/ExpressError.js
@@ -0,0 +1,10 @@
+// Config the class to create a error and displays it
+class ExpressError extends Error {
+  constructor(message, statusCode) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ExpressError;
